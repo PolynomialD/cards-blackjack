17de01a26c4e1eeb8f79ba46a9af14f61f88cb81
polynomiald <steven.grindey@gmail.com>
2019-08-26 10:58:29 +0100

    cards length test passing

--- src/Deck.ts
+++ src/Deck.ts
@@ -1,21 +1,26 @@
 export class Deck {
-  public cards: never[]
+  readonly cards: object[]
   constructor () {
     this.cards = this.buildCards()
   }
 
   buildCards () {
-    const suits = [['clubs', '♣'], ['diamonds', '♦'], ['hearts', '♥'], ['spades', '♠']]
+    const suits = ['clubs', 'diamonds', 'hearts', 'spades']
     const values = [['2', 2],['3',3],['4', 4],['5', 5],['6', 6],['7', 7],['8', 8],['9', 9],['10', 10],['jack', 10],['queen', 10],['king', 10],['ace', 11]]
-    const cards = suits.map((suit) => {
+    const cards: cards = suits.map((suit) => {
       return values.map((value) => {
         return {
-          faceValue: `${value[0]} + ${suit[1]}`,
-          trueValue: value[1],
-          // image: require(`../../assets/images/cards/${value[0]}_of_${suit[0]}.png`)
+          face: `${value[0]} of ${suit}`,
+          value: value[1]
+          // image: `../../assets/images/cards/${value[0]}_of_${suit}.png`
         }
       })
     })
     return [].concat.apply([], cards)
   }
 }
+
+type cards = {
+  readonly face: string,
+  readonly value: string | number
+}[][]
